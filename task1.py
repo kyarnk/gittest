ac6c2e3fc2c204e33069ea3e64dc0103066233ea
Nikita K <nk47blue@gmail.com>
2024-09-06 12:30:09 +0300

    Add textfile1 and make few changes in task1

--- task1.py
+++ task1.py
@@ -1,3 +1,12 @@
+#-----------------
+# programm by Nikita.K.
+
+# version          date     info
+# 1.0              2024    initial version
+
+
+
+
 name = input("Введите ваше имя:")
 age = input("Введите ваш возраст:")
 сolor = input("Введите любимый цвет:")
